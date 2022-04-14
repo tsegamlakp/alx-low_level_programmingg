91b7015bcf88a57a1d3d38442ea0fe5abf52b020
tsegamlakp <99799402+tsegamlakp@users.noreply.github.com>
2022-04-14 22:10:47 +0300

    fixed

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,28 +1,79 @@
-#ifndef VARF
-#define VARF
-#ifndef VARIADIC_FUNCTIONS_H_
-#define VARIADIC_FUNCTIONS_H_
-
+#include "variadic_functions.h"
 #include <stdarg.h>
-
+#include <stdio.h>
 /**
- * struct printer - A new struct type defining a printer.
- * @symbol: A symbol representing a data type.
- * @print: A function pointer to a function that prints
- *         a data type corresponding to symbol.
+ * print_s -print string
+ * @list: arg list
+ *
+ * Description: print str
  */
-
-typedef struct printer
+void print_s(va_list list)
 {
-	char *symbol;
-	void (*print)(va_list arg);
+	char *s;
 
-} printer_t;
+	s = va_arg(list, char *);
+	if (s == NULL)
+		printf("(nil)");
+	printf("%s", s);
+}
+/**
+ * print_i -pprint integer
+ * @list: arg list
+ *
+ * Description: print int
+ */
+void print_i(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+/**
+ * print_f -print float
+ * @list: arg list
+ *
+ * Description: print float
+ */
+void print_f(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+/**
+ * print_c -print char
+ * @list: arg list
+ *
+ * Description: print char
+ */
+void print_c(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+/**
+ * print_all -print every thing
+ * @format: format string
+ *
+ * Description: print string based on format
+ */
+void print_all(const char *const format, ...)
+{
+	char fmt[] = {'c', 'i', 'f', 's'};
+	int i, j;
+	va_list args;
+	void (*ptr_print[])(va_list) = {print_c, print_i, print_f, print_s};
 
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
-
-
-#endif
+	va_start(args, format);
+	i = j = 0;
+	while (format[i] != '\0')
+	{
+		while ((format[i] != fmt[j]) && j < 4)
+			j++;
+		if (j < 4)
+		{
+			ptr_print[j](args);
+		}
+		/*printf("\tj = %d \tformat[%d + 1] = %c\t\n", j, i, format[i + 1]);*/
+		if (!((j < 4 && format[i + 1] == '\0') || j >= 4))
+			printf(", ");
+		i++;
+		j = 0;
+	}
+	printf("\n");
+}
