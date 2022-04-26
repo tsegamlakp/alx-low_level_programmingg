13693b37e363853927d63bbb7e38067354c327b1
tsegamlakp <99799402+tsegamlakp@users.noreply.github.com>
2022-04-26 18:59:33 +0300

    Add files via upload

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
-
-/**
- * Desc: header file containinf prototypes and definitions
- * for all functions and types written in this proj
- */
-
-#include <stdlib.h>
-
+#include<stddef.h>
+#include<stdarg.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+/*Structs*/
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -23,11 +21,11 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-
+/*Prototypes*/
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-#endif /* LISTS_H */
+#endif
