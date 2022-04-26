9b9728196ae321316d3e3c73b6c6b3be8e5c75df
tsegamlakp <99799402+tsegamlakp@users.noreply.github.com>
2022-04-26 18:48:31 +0300

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,10 @@
 #ifndef _LISTS_H_
-#define _LISTS_H_
+#define LISTS_H
+#include<stddef.h>
+#include<stdarg.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
