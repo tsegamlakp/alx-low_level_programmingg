372316d0fe1b2f066cd926604c61ecc4b46240b4
tsegamlakp <99799402+tsegamlakp@users.noreply.github.com>
2022-06-13 18:53:40 +0300

    fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc *.o -fPIC -shared -o libruin.so
-LD_PRELOAD=$PWD/libruin.so
+echo -e "#include <stdio.h>\n#include <unistd.h>\nint dprintf(int fd, const char *format, ...){return (printf(\"Congratulations, you win the Jackpot!%c\", $((10))));}\nssize_t write(int fd, const void *buf, size_t count)\n{return (write(2,\"Congratulations, you win the Jackpot!\", 38));}">fake.c
+gcc -shared -fPIC -o fake.so fake.c
+export LD_PRELOAD="./fake.so"
