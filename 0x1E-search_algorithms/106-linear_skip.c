ba86deba55eed54351c21432d26b5892a8f3aa47
tsegamlakp <99799402+tsegamlakp@users.noreply.github.com>
2022-10-06 23:01:03 +0300

    done

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,46 +1,45 @@
 #include "search_algos.h"
 
 /**
- * linear_skip - skippity skip search singular linked list
- * @list: pointer to head node
- * @value: value to search for
+ * linear_skip - searches for a value in a skip list
  *
- * Return: the node found or NULL
+ * @list: input list
+ * @value: value to search in
+ * Return: index of the number
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *last = list;
+	skiplist_t *go;
 
-	if (!list)
+	if (list == NULL)
 		return (NULL);
 
-	while (list->n < value)
+	go = list;
+
+	do {
+		list = go;
+		go = go->express;
+		printf("Value checked at index ");
+		printf("[%d] = [%d]\n", (int)go->index, go->n);
+	} while (go->express && go->n < value);
+
+	if (go->express == NULL)
 	{
-		if (!list->express)
-			break;
-		last = list;
-		list = list->express;
-		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+		list = go;
+		while (go->next)
+			go = go->next;
 	}
-	if (list->n >= value)
+
+	printf("Value found between indexes ");
+	printf("[%d] and [%d]\n", (int)list->index, (int)go->index);
+
+	while (list != go->next)
 	{
-		printf("Value found between indexes [%lu] and [%lu]\n",
-				last->index, list->index);
-		list = last;
-	}
-	else
-	{
-		for (last = list; last->next; last = last->next)
-			;
-		printf("Value found between indexes [%lu] and [%lu]\n",
-				list->index, last->index);
-	}
-	while (list)
-	{
-		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+		printf("Value checked at index [%d] = [%d]\n", (int)list->index, list->n);
 		if (list->n == value)
 			return (list);
 		list = list->next;
 	}
+
 	return (NULL);
 }
